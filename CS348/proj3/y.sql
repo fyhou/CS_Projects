b74e7bacec1dc7dd80509bd7c105606398526ed2
Joe Martella <martellaj@gmail.com>
2011-10-30 17:08:10 -0400

    pro_department_report: sucessfully prints names of each department and number of employees it has

--- CS348/proj3/y.sql
+++ CS348/proj3/y.sql
@@ -7,12 +7,23 @@ set serveroutput on size 32000
 create or replace procedure pro_department_report 
 is
 	cursor departments is
-		select deptid, dname from department;
+		select deptid, dname
+		from department 
+		order by dname;
 	d departments%rowtype;
+	profCount number(3);
 begin
 	for d in departments
 	loop
-		dbms_output.put_line(d.dname);
+		dbms_output.put_line('Department: ' || d.dname);
+
+		select count(1)
+		into profCount
+		from faculty
+		where faculty.deptid=d.deptid;
+		dbms_output.put_line('Total faculty members: ' || profCount);
+
+		dbms_output.put_line('-------------' || CHR(10));
 	end loop;
 end;
 /
