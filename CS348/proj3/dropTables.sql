58762917cf716d91f87b097eaae2af82ada4813d
Joe Martella <martellaj@gmail.com>
2011-10-30 15:44:49 -0400

    Added create and drop tables scripts.

---
+++ CS348/proj3/dropTables.sql
@@ -0,0 +1,9 @@
+-- CS 348: Project 3    --
+-- Author: Joe Martella --
+-- Date: 11/08/2011     --
+
+drop table student;
+drop table class;
+drop table faculty;
+drop table department;
+drop table enrolled;
