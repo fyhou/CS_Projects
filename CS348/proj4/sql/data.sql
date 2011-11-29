5d23e0b8bd9158a02f8a2555922f00e1f4cd1987
martellaj <martellaj@gmail.com>
2011-11-29 14:19:32 -0500

    added more data to fill

--- CS348/proj4/sql/data.sql
+++ CS348/proj4/sql/data.sql
@@ -11,3 +11,12 @@ insert into student values (3,'S3','BS','FR', 'pw3');
 insert into faculty values (101,'F1', 'pw4');
 insert into faculty values (102,'F2', 'pw5');
 insert into faculty values (103,'F3', 'pw6');
+
+insert into class values ('CS100', 'F', 2012, to_char('09:30', 'HH:MI'), "X1", 101);
+insert into class values ('CS200', 'F', 2012, to_char('10:30', 'HH:MI'), "X1", 101);
+
+insert into evaluation values ('Homework 1', 'HW', 20, to_char('12/12/12', 'MM/DD/YY'), 'R1', 'CS100'); 
+insert into evaluation values ('Homework 2', 'HW', 20, to_char('12/19/12', 'MM/DD/YY'), 'R1', 'CS100'); 
+
+insert into grade values (1, 'CS100', 90, 'Homework 1');
+insert into grade values (1, 'CS100', 100, 'Homework 2');
