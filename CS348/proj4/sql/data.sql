fb0d0be68a40d71e721c74828c177f72a8dbe246
martellaj <martellaj@gmail.com>
2011-11-29 14:27:07 -0500

    to_char =!= to_date

--- CS348/proj4/sql/data.sql
+++ CS348/proj4/sql/data.sql
@@ -12,11 +12,11 @@ insert into faculty values (101,'F1', 'pw4');
 insert into faculty values (102,'F2', 'pw5');
 insert into faculty values (103,'F3', 'pw6');
 
-insert into class values ('CS100', 'F', 2012, to_char('09:30', 'HH:MI'), 'X1', 101);
-insert into class values ('CS200', 'F', 2012, to_char('10:30', 'HH:MI'), 'X1', 101);
+insert into class values ('CS100', 'F', 2012, to_date('09:30', 'HH:MI'), 'X1', 101);
+insert into class values ('CS200', 'F', 2012, to_date('10:30', 'HH:MI'), 'X1', 101);
 
-insert into evaluation values ('Homework 1', 'HW', 20, to_char('12/12/12', 'MM/DD/YY'), 'R1', 'CS100'); 
-insert into evaluation values ('Homework 2', 'HW', 20, to_char('12/19/12', 'MM/DD/YY'), 'R1', 'CS100'); 
+insert into evaluation values ('Homework 1', 'HW', 20, to_date('12/12/12', 'MM/DD/YY'), 'R1', 'CS100'); 
+insert into evaluation values ('Homework 2', 'HW', 20, to_date('12/19/12', 'MM/DD/YY'), 'R1', 'CS100'); 
 
 insert into grade values (1, 'CS100', 90, 'Homework 1');
 insert into grade values (1, 'CS100', 100, 'Homework 2');
