262aec3cac816873d13774794a746b13cfa54870
martellaj <martellaj@gmail.com>
2011-11-29 21:32:46 -0500

    implemented Comparable so it can be sorted

--- CS348/proj4/src/Evaluation.java
+++ CS348/proj4/src/Evaluation.java
@@ -1,9 +1,20 @@
 
-public class Evaluation {
+public class Evaluation implements Comparable<Evaluation>{
 	String name;
 	String type;
 	String weight;
 	String deadline;
 	String room;
 	String cname;
+	
+	@Override
+	public int compareTo(Evaluation e) {
+		// TODO Auto-generated method stub
+		if (this.cname.compareTo(e.cname) > 0)
+			return 1;
+		else if (this.cname.compareTo(e.cname)< 0)
+			return -1;
+		else 
+			return 0;
+	}
 }
