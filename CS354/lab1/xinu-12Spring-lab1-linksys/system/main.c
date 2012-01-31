3272ddf0e71820d9126c2feb6b0b1ae0c032eaf1
Joe Martella <martellaj@gmail.com>
2012-01-31 15:22:07 -0500

    Lab completed

--- CS354/lab1/xinu-12Spring-lab1-linksys/system/main.c
+++ CS354/lab1/xinu-12Spring-lab1-linksys/system/main.c
@@ -11,7 +11,20 @@
 
 int main(int argc, char **argv)
 {
-	kprintf("Hello World!\n");
+	void goToBed(void);
+
+	resume(create(goToBed, 1000, 20, "process 1", 0));
+	resume(create(goToBed, 950, 20, "process 2", 0));
+
+	pid32 i = 0;
+	for (i = 0; i < NPROC-1; i++) {
+		print_proc_info(i);
+	}
 	
 	return OK;
+}
+
+void goToBed(void) 
+{
+	sleep(5000);
 }
--- CS354/lab1/xinu-12Spring-lab1-linksys/system/print_proc_info.c
+++ CS354/lab1/xinu-12Spring-lab1-linksys/system/print_proc_info.c
@@ -1,28 +1,53 @@
 /* print_proc_info.c - print_proc_info */
- 2
- 3     #include < xinu.h >
- 4     #include < string.h >
- 5
- 6     /*------------------------------------------------------------------------
- 7     *  print_proc_info  -  Prints the info in proctab for a given pid
- 8     *------------------------------------------------------------------------
- 9     */
-10     syscall print_proc_info(
-11         pid32   pid   /* process ID     */
-12     )
-13     {
-14     intmask mask;     /* saved interrupt mask   */
-15
-16           /*-----  Declare variables to hold values  ------*/
-17           /*-----  ------------------------------- --------*/
-18
-19     mask = disable();
-20
-21           /*-----  Your code for fetching process specific info and displaying them goes here -----*/
-22           /*-----  ------------------------------- --------*/
-23           /*-----  ------------------------------- --------*/
-24
-25
-26     restore(mask);
-27
-28     }
+
+     #include <xinu.h>
+     #include <string.h>
+
+     /*------------------------------------------------------------------------
+      *  print_proc_info  -  Prints the info in proctab for a given pid
+      *------------------------------------------------------------------------
+      */
+     syscall print_proc_info(
+         pid32   pid   /* process ID     */
+     )
+     {
+     	intmask mask;     /* saved interrupt mask   */
+
+        /*-----  Declare variables to hold values  ------*/
+        /*-----  ------------------------------- --------*/
+	  	char		*name;	    /* process name (for debugging)	*/
+		pri16		priority;	/* process priority		*/
+		uint32	ssize;		    /* stack size in bytes		*/
+		char	*saddr;			/* stack address		*/
+
+     	mask = disable();
+
+        /*-----  Your code for fetching process specific info and displaying them goes here -----*/
+        /*-----  ------------------------------- --------*/
+        /*-----  ------------------------------- --------*/
+
+		// get the process' table entry
+		struct	procent *prptr;		/* ptr to process' table entry	*/
+		prptr = &proctab[pid];
+
+		if (isbadpid(pid) || (pid == NULLPROC)
+	    || ((prptr = &proctab[pid])->prstate) == PR_FREE) {
+		restore(mask);
+		return SYSERR;
+		}
+
+		name     = prptr->prname;
+		priority = prptr->prprio;
+		ssize    = prptr->prstklen; 
+		saddr    = prptr->prstkptr;
+	
+		kprintf("Process name: %s\n\r", name);
+		kprintf("Process priority: %d\n\r", priority);
+		kprintf("Process stack size: %d\n\r", ssize);
+		kprintf("Process stack address: %x\n\r\n\r", saddr);
+
+     	restore(mask);
+		
+		return OK;
+     }
+	
