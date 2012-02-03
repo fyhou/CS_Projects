a1f25c0043fc532d78e0e8e417f48354f628c18a
Joe Martella <martellaj@gmail.com>
2012-02-03 09:43:33 -0500

    lab 1 completed

--- CS354/lab1/xinu-12Spring-lab1-linksys/system/main.c
+++ CS354/lab1/xinu-12Spring-lab1-linksys/system/main.c
@@ -13,8 +13,8 @@ int main(int argc, char **argv)
 {
 	void goToBed(void);
 
-	resume(create(goToBed, 1000, 20, "process 1", 0));
-	resume(create(goToBed, 950, 20, "process 2", 0));
+	//resume(create(goToBed, 1000, 20, "process 1", 0));
+	//resume(create(goToBed, 950, 20, "process 2", 0));
 
 	pid32 i = 0;
 	for (i = 0; i < NPROC-1; i++) {
