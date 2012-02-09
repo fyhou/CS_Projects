8686f438f861f2dbb0d9cbd358ba33817d62b64a
Joe Martella <martellaj@gmail.com>
2012-02-09 11:06:23 -0500

    first commit of CS 354 lab 2

---
+++ CS354/lab2/xinu-12Spring-lab1-linksys/system/main.c
@@ -0,0 +1,17 @@
+/*  main.c  - main */
+
+#include <xinu.h>
+#include <stdio.h>
+
+/************************************************************************/
+/*									*/
+/* main - main program for testing Xinu					*/
+/*									*/
+/************************************************************************/
+
+int main(int argc, char **argv)
+{
+	kprintf("Hello World!\n");
+	
+	return OK;
+}
