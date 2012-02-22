7a07156bb5a735c4a8857979ba91fa632358fdd6
Joe Martella <martellaj@gmail.com>
2012-02-22 15:25:06 -0500

    Update CS354/lab3/xinu-12Spring-lab3-linksys/system/pipread.c

--- CS354/lab3/xinu-12Spring-lab3-linksys/system/pipread.c
+++ CS354/lab3/xinu-12Spring-lab3-linksys/system/pipread.c
@@ -1,24 +1,24 @@
 #include <xinu.h>
 
 syscall pipread(int32 pip, char *buf, uint32 len) {
-  intmask mask;
+ 	intmask mask;
 	struct pipe *pipeptr;
-	kprintf("err1\n\r");
+
 	mask = disable();
 
 	if (isbadpipeid(pip) || (pipeptr = &pipelist[pip])->pipestate != PIPE_CONNECTED || (pipeptr = &pipelist[pip])->end1 != getpid()) {
 		restore(mask);
 		return(SYSERR);
 	}
-	kprintf("err2\n\r");
+
 	pipeptr = &pipelist[pip];
 	int i = 0;  // keep track of length
 	int pos = pipeptr->pos;
-	kprintf("err3\n\r");
+
 	int charInBuff = pos;
 	if (charInBuff == 0) 
 	{
-		kprintf("err4\n\r");
+
 		for (i = 0; i < len; i++)
 		{
 			wait(pipeptr->csem);
@@ -33,7 +33,6 @@ syscall pipread(int32 pip, char *buf, uint32 len) {
 	}
 	else if (charInBuff <= len)
 	{
-		kprintf("err5\n\r");
 		for (i = 0; i < charInBuff; i++)
 		{
 			//kprintf("PIPREAD (A): char = %c\n\r", pipeptr->buffer[i]);
@@ -41,24 +40,23 @@ syscall pipread(int32 pip, char *buf, uint32 len) {
 			pos--; 
 			pipeptr->pos = pos;
 		}
-		kprintf("err6\n\r");
+		
 		semtab[pipeptr->csem].scount = 0;
 		semtab[pipeptr->psem].scount = PIPE_SIZE;
-		kprintf("err7\n\r");
+		
 		restore(mask);
 		return(charInBuff);
 	}
 	else 
 	{
-		kprintf("err8\n\r");
 		for (i = 0; i < len; i++)
 		{
 			wait(pipeptr->csem);
-				kprintf("PIPREAD (B): char = %c\n\r", pipeptr->buffer[i]);
+				//kprintf("PIPREAD (B): char = %c\n\r", pipeptr->buffer[i]);
         			buf[i] = pipeptr->buffer[i];
 				pos--;
 				pipeptr->pos = pos;
-			signaln(pipeptr->psem);
+			signal(pipeptr->psem);
 		}
 
 		restore(mask);
