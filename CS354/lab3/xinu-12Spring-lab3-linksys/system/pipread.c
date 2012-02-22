c24149a018b793892b76fdb4f391b9be5368d361
Joe Martella <martellaj@gmail.com>
2012-02-21 21:22:47 -0500

    Update CS354/lab3/xinu-12Spring-lab3-linksys/system/pipread.c

--- CS354/lab3/xinu-12Spring-lab3-linksys/system/pipread.c
+++ CS354/lab3/xinu-12Spring-lab3-linksys/system/pipread.c
@@ -1,1 +1,64 @@
-syscall
+#include <xinu.h>
+
+syscall pipread(int32 pip, char *buf, uint32 len) {
+  intmask mask;
+	struct pipe *pipeptr;
+
+	mask = disable();
+
+	// add || (pipeptr = &pipelist[pip])->end1 != getpid() to condition when done
+	if (isbadpipeid(pip) || (pipeptr = &pipelist[pip])->pipestate != PIPE_CONNECTED || (pipeptr = &pipelist[pip])->ownerPID != getpid()) {
+		restore(mask);
+		return(SYSERR);
+	}
+
+	pipeptr = &pipelist[pip];
+	int i = 0;  // keep track of length
+	int pos = pipeptr->pos;
+
+	int charInBuff = pos;
+	if (charInBuff == 0) 
+	{
+		for (i = 0; i < len; i++)
+		{
+			wait(pipeptr->csem);
+        buf[i] = pipeptr->buffer[i];
+				pos--; 
+				pipeptr->pos = pos;
+			signal(pipeptr->psem);
+		}
+
+		restore(mask);
+		return(len);
+	}
+	else if (charInBuff <= len)
+	{
+		for (i = 0; i < charInBuff; i++)
+		{
+
+			pipeptr->buffer[i] = buf[i];
+			pos--; 
+			pipeptr->pos = pos;
+		}
+
+		semtab[pipeptr->csem].scount = 0;
+		semtab[pipeptr->psem].scount = PIPE_SIZE;
+
+		restore(mask);
+		return(charInBuff);
+	}
+	else 
+	{
+		for (i = 0; i < len; i++)
+		{
+			wait(pipeptr->csem);
+        buf[i] = pipeptr->buffer[i];
+				pos--;
+				pipeptr->pos = pos;
+			signal(pipeptr->psem);
+		}
+
+		restore(mask);
+		return(len);
+	}
+}
