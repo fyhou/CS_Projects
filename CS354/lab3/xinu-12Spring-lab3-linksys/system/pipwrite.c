fae9393c2fd23a382e9bda5f7aa36844ecf7d0ad
Joe Martella <martellaj@gmail.com>
2012-02-22 14:51:39 -0500

    Update CS354/lab3/xinu-12Spring-lab3-linksys/system/pipwrite.c

--- CS354/lab3/xinu-12Spring-lab3-linksys/system/pipwrite.c
+++ CS354/lab3/xinu-12Spring-lab3-linksys/system/pipwrite.c
@@ -7,6 +7,22 @@ syscall pipwrite(int32 pip, char *buf, uint32 len) {
 	mask = disable();
 
 	if (isbadpipeid(pip) || (pipeptr = &pipelist[pip])->pipestate != PIPE_CONNECTED || (pipeptr = &pipelist[pip])->ownerPID != getpid() || (pipeptr = &pipelist[pip])->end2 != getpid()) {
+		
+		if (isbadpipeid(pip)) 
+		{
+			kprintf("err1\r\n");
+		}
+		if ((pipeptr = &pipelist[pip])->pipestate != PIPE_CONNECTED)
+		{
+			kprintf("err2\r\n");
+		}
+		if (pipeptr = &pipelist[pip])->ownerPID != getpid()) {
+			kprintf("err3\r\n");
+		}
+		if ((pipeptr = &pipelist[pip])->end2 != getpid()) {
+			kprintf("err4\r\n");
+		}
+		
 		restore(mask);
 		return(SYSERR);
 	}
