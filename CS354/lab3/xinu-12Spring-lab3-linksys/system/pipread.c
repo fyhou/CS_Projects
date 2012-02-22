020923270a73b8b8f55b9cf2e78c78223134c428
Joe Martella <martellaj@gmail.com>
2012-02-21 21:46:32 -0500

    Update CS354/lab3/xinu-12Spring-lab3-linksys/system/pipread.c

--- CS354/lab3/xinu-12Spring-lab3-linksys/system/pipread.c
+++ CS354/lab3/xinu-12Spring-lab3-linksys/system/pipread.c
@@ -16,7 +16,7 @@ syscall pipread(int32 pip, char *buf, uint32 len) {
 	int i = 0;  // keep track of length
 	int pos = pipeptr->pos;
 
-	int charInBuff = pos + 1;
+	int charInBuff = pos;
 	if (charInBuff == 0) 
 	{
 		for (i = 0; i < len; i++)
