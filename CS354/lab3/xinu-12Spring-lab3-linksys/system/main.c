5cdd23ded5989b0df4b02a03d5198687a8a02003
Joe Martella <martellaj@gmail.com>
2012-02-22 14:37:21 -0500

    Update CS354/lab3/xinu-12Spring-lab3-linksys/system/main.c

--- CS354/lab3/xinu-12Spring-lab3-linksys/system/main.c
+++ CS354/lab3/xinu-12Spring-lab3-linksys/system/main.c
@@ -19,17 +19,14 @@ int main(int argc, char **argv)
 	
 	pid32 end1, end2;
 
-	end2 = create(produce, 1000, 20, "producer", 0);  // writer
-	end1 = create(consume, 1000, 20, "consumer", 0);  // reader
+	resume(end2 = create(produce, 1000, 20, "producer", 0));  // writer
+	resume(end1 = create(consume, 1000, 20, "consumer", 0));  // reader
 	
 	kprintf("PIDs = %d, %d\n\r", end2, end1);
 	
 	x = pipcreate();
 	int result = pipconnect(x, end1, end2);
 	kprintf("Pipe connection resulted in: %d\n\r", result);
-	
-	resume(end2);
-	resume(end1);
 	
 	char c;
 
