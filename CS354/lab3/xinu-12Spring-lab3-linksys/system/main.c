958c31fddc11a66021b7471130bed8031d0f8b49
Joe Martella <martellaj@gmail.com>
2012-02-23 16:18:15 -0500

    Update CS354/lab3/xinu-12Spring-lab3-linksys/system/main.c

--- CS354/lab3/xinu-12Spring-lab3-linksys/system/main.c
+++ CS354/lab3/xinu-12Spring-lab3-linksys/system/main.c
@@ -36,12 +36,12 @@ void produce(void)
 {
 	int i = 0;
 	char buff[2];
-	for (i = 0; i < 100; i++) 
+	for (i = 0; i < 300; i++) 
 	{
 		if (i < 10) sprintf(buff, "0%d\n\r", i);
 		else sprintf(buff, "%d\n\r", i);
 		
-		int result = pipwrite(x, buff, 4);
+		int result = pipwrite(x, buff, 2);
 		if (result == -1) kprintf("pipwrite error\r\n");
 	}
 }
@@ -54,9 +54,9 @@ void consume(void)
 		int i = 0;
 		for (i = 0; i < N; i++) 
 		{
-			int result = pipread(x, buff, 4);
+			int result = pipread(x, buff, 2);
 			if (result == -1) kprintf("pipread error\r\n");
-			kprintf("Read from pipe: %s", buff);
+			kprintf("Read from pipe: %s\n\r", buff);
 		}
 		//suspend(getpid());
 	}
