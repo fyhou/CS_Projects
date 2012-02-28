7dce9f23b582a8c55d1c4b98ae9b9a56bba90e93
Joe Martella <martellaj@gmail.com>
2012-02-28 14:25:34 -0500

    Update CS354/lab3/xinu-12Spring-lab3-linksys/system/pipread.c

--- CS354/lab3/xinu-12Spring-lab3-linksys/system/pipread.c
+++ CS354/lab3/xinu-12Spring-lab3-linksys/system/pipread.c
@@ -43,10 +43,6 @@ syscall pipread(int32 pip, char *buf, uint32 len) {
 		for (i = 0; i < len; i++)
 		{
 			wait(pipeptr->csem);
-				//if (isbadpipeid(pip)) {
-				//	return SYSERR;
-				//}
-				
         			buf[i] = pipeptr->buffer[readPos];
 				readPos++; 
 				
@@ -63,11 +59,6 @@ syscall pipread(int32 pip, char *buf, uint32 len) {
 		for (i = 0; i < charInBuff; i++)
 		{
 			wait(pipeptr->csem);
-				//if (isbadpipeid(pip)) {
-				//	return SYSERR;
-				//}
-				
-				//kprintf("PIPREAD (A): char = %c\n\r", pipeptr->buffer[i]);
 				buf[i] = pipeptr->buffer[readPos];
 				readPos++; 
 				
@@ -84,11 +75,6 @@ syscall pipread(int32 pip, char *buf, uint32 len) {
 		for (i = 0; i < len; i++)
 		{
 			wait(pipeptr->csem);
-				//if (isbadpipeid(pip)) {
-				//	return SYSERR;
-				//}
-				
-				//kprintf("PIPREAD (B): char = %c\n\r", pipeptr->buffer[i]);
         			buf[i] = pipeptr->buffer[readPos];
 				readPos++;
 				
