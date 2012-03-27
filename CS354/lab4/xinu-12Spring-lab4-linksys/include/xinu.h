fc3b0049a3d2c3fa1b80b71fd04b9720a0acdfc0
Joe Martella <martellaj@gmail.com>
2012-03-27 14:10:49 -0400

    Initial commit of CS354 lab 4

---
+++ CS354/lab4/xinu-12Spring-lab4-linksys/include/xinu.h
@@ -0,0 +1,23 @@
+/* xinu.h - include all system header files */
+
+#include <kernel.h>
+#include <conf.h>
+#include <process.h>
+#include <queue.h>
+#include <sched.h>
+#include <semaphore.h>
+#include <memory.h>
+#include <bufpool.h>
+#include <clock.h>
+#include <mark.h>
+#include <ports.h>
+#include <uart.h>
+#include <tty.h>
+#include <device.h>
+#include <interrupt.h>
+#include <ag71xx.h>
+#include <mips.h>
+#include <gpio.h>
+#include <date.h>
+#include <shell.h>
+#include <prototypes.h>
---
+++ CS354/lab4/xinu-12Spring-lab4-linksys/system/main.c
@@ -0,0 +1,28 @@
+/*  main.c  - main */
+
+#include <xinu.h>
+#include <stdio.h>
+
+/************************************************************************/
+/*									*/
+/* main - main program for testing Xinu					*/
+/*									*/
+/************************************************************************/
+
+int main(int argc, char **argv)
+{
+	umsg32 retval;
+
+	/* Creating a shell process */
+
+	resume(create(shell, 4096, 1, "shell", 1, CONSOLE));
+
+	retval = recvclr();
+	while (TRUE) {
+		retval = receive();
+		kprintf("\n\n\rMain process recreating shell\n\n\r");
+		resume(create(shell, 4096, 1, "shell", 1, CONSOLE));
+	}
+
+	return OK;
+}
