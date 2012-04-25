806760c736d5b320725ee7adae45a31511fd804f
Joe Martella <martellaj@gmail.com>
2012-04-24 22:04:53 -0300

    Update CS354/lab6/xinu-12Spring-lab6-linksys/system/lflistdir.c

--- CS354/lab6/xinu-12Spring-lab6-linksys/system/lflistdir.c
+++ CS354/lab6/xinu-12Spring-lab6-linksys/system/lflistdir.c
@@ -1,8 +1,8 @@
 #include <xinu.h>
 
 syscall  lflistdir(
-	  did32		descrp		/* descriptor for device	*/
-	)
+  did32		descrp		/* descriptor for device	*/
+)
 {
 	intmask		mask;		/* saved interrupt mask		*/
 	struct dentry	*devptr;	/* entry in device switch table	*/
@@ -12,31 +12,29 @@ syscall  lflistdir(
 		restore(mask);
 		return SYSERR;
 	}
-  
+
 	devptr = (struct dentry *) &devtab[descrp];
-  
-  if (devptr->dvnum == 4) {
-    kprintf("there are %d files right now, nigga\n\r", Lf_data.lf_dir.lfd_nfiles);
-    
-    if (Lf_data.lf_dir.lfd_nfiles == 0) {
-    	kprintf("No files");
-    	restore(mask);
-    	return OK;
-    }
-    
-    int i = 0;
-    for (i = 0; i < LF_NUM_DIR_ENT; i++) { 	
-    	if (Lf_data.lf_dir.lfd_files[i].ld_ilist != NULL) {
-    		kprintf("%s\n\r", Lf_data.lf_dir.lfd_files[i].ld_name);
-    	}
-    }
-  }
-  // if the device ain't quite right
-  else {
-  	restore(mask);
-    return SYSERR;
-  }
-  
+
+	if (devptr->dvnum == 4) {
+		if (Lf_data.lf_dir.lfd_nfiles == 0) {
+			kprintf("No files\n\r");
+			restore(mask);
+			return OK;
+		}
+
+		int i = 0;
+		for (i = 0; i < LF_NUM_DIR_ENT; i++) { 	
+			if (Lf_data.lf_dir.lfd_files[i].ld_ilist != NULL) {
+				kprintf("%s\n\r", Lf_data.lf_dir.lfd_files[i].ld_name);
+			}
+		}
+	}
+	// if the device ain't quite right
+	else {
+		restore(mask);
+		return SYSERR;
+	}
+
 	restore(mask);
 	return OK;
 }
