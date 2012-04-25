a6f4e54312af605e5e905672662beaea20a4f79b
Joe Martella <martellaj@gmail.com>
2012-04-24 21:59:14 -0300

    Update CS354/lab6/xinu-12Spring-lab6-linksys/system/lflistdir.c

--- CS354/lab6/xinu-12Spring-lab6-linksys/system/lflistdir.c
+++ CS354/lab6/xinu-12Spring-lab6-linksys/system/lflistdir.c
@@ -19,23 +19,25 @@ syscall  lflistdir(
   if (devptr->dvnum == 4) {
     kprintf("there are %d files right now, nigga\n\r", Lf_data.lf_dir.lfd_nfiles);
     
+    if (Lf_data.lf_dir.lfd_nfiles == 0) {
+    	kprintf("No files");
+    	restore(mask);
+    	return OK;
+    }
+    
     int i = 0;
-    int gottaFileYo = 0;
     for (i = 0; i < LF_NUM_DIR_ENT; i++) { 	
     	if (Lf_data.lf_dir.lfd_files[i].ld_ilist != IB_NULL) {
     		kprintf("%s\n\r", Lf_data.lf_dir.lfd_files[i].ld_name);
-    		gottaFileYo++;
     	}
     }
-    
-    if (gottaFileYo == 0)
-    	kprintf("No files");
   }
   // if the device ain't quite right
   else {
+  	restore(mask);
     return SYSERR;
   }
   
 	restore(mask);
-	return retval;
+	return OK;
 }
