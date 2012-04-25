0e316f3edf6fee4317b40c73d80028f8271cb6f9
Joe Martella <martellaj@gmail.com>
2012-04-24 21:38:00 -0300

    Update CS354/lab6/xinu-12Spring-lab6-linksys/system/lflistdir.c

--- CS354/lab6/xinu-12Spring-lab6-linksys/system/lflistdir.c
+++ CS354/lab6/xinu-12Spring-lab6-linksys/system/lflistdir.c
@@ -1,1 +1,28 @@
-test
+#include <xinu.h>
+
+syscall  lflistdir(
+	  did32		descrp,		/* descriptor for device	*/
+	)
+{
+	intmask		mask;		/* saved interrupt mask		*/
+	struct dentry	*devptr;	/* entry in device switch table	*/
+	int32		retval;		/* value to return to caller	*/
+
+	mask = disable();
+	if (isbaddev(descrp)) {
+		restore(mask);
+		return SYSERR;
+	}
+  
+	devptr = (struct dentry *) &devtab[descrp];
+  
+  if (devptr->dvnum == 4) {
+    kprintf("fuck you\n\r");
+  }
+  else {
+    kprintf("fuck you twice\n\r");
+  }
+  
+	restore(mask);
+	return retval;
+}
