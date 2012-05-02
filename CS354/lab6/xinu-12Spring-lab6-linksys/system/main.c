0ca6aac59c1726097dbd5903b7fa5d6bc03ae5ea
Joe Martella <martellaj@gmail.com>
2012-05-02 19:38:18 -0300

    Update CS354/lab6/xinu-12Spring-lab6-linksys/system/main.c

--- CS354/lab6/xinu-12Spring-lab6-linksys/system/main.c
+++ CS354/lab6/xinu-12Spring-lab6-linksys/system/main.c
@@ -14,6 +14,8 @@ int main(int argc, char **argv)
 	int32	retval;
 	int i; 
 	did32 file = 0;
+	
+	int result;
 
 	/* Obtain network configuration from DHCP server */
 
@@ -31,61 +33,38 @@ int main(int argc, char **argv)
 	kprintf("\n\r**********************************************************\n\r");
 
 	/* Creating a shell process */
-
-	for(i=0; i<Nlfl; i++){
-		if(lfltab[i].lfstate == LF_USED){
-			kprintf("Files open on this system, cannot format\r\n");
-			return SYSERR;
-		}
-	}                        
-
-	if(lfscreate(Lf_data.lf_dskdev, 100, 500*512) == SYSERR){
-		kprintf("Creating the filesystem failed\r\n");
-		return SYSERR;
-	}
-
-	/**
-	 * CALL LS WITH NO FILES CREATED
-	 */
-	if (lflistdir(4) == SYSERR) {
-		kprintf("SYSERR handled.\n\r");
-	}
 	
-	kprintf("\n\r");
+	for(i=0; i<Nlfl; i++){
+             if(lfltab[i].lfstate == LF_USED){
+                   kprintf("Files open on this system, cannot format\r\n");
+                    return SYSERR;
+             }
+        }                        
 
-	/**
-	 * CREATE FOUR FILES
-	 */
-	file = open(LFILESYS, "I", "rw");
-	if(file == SYSERR){
-		kprintf("File open failed for /%d\r\n", i);
-		return SYSERR;
-	}
+        if(lfscreate(Lf_data.lf_dskdev, 100, 500*512) == SYSERR){
+                               kprintf("Creating the filesystem failed\r\n");
+                                return SYSERR;
+                        }
 
-	file = open(LFILESYS, "LOVE", "rw");
-	if(file == SYSERR){
-		kprintf("File open failed for /%d\r\n", i);
-		return SYSERR;
-	}
-
-	file = open(LFILESYS, "CS 354", "rw");
-	if(file == SYSERR){
-		kprintf("File open failed for /%d\r\n", i);
-		return SYSERR;
-	}
-
-	file = open(LFILESYS, "LABS", "rw");
-	if(file == SYSERR){
-		kprintf("File open failed for /%d\r\n", i);
-		return SYSERR;
-	}
-
-	/**
-	 * CALL LS AGAIN
-	 */
-	if (lflistdir(4) == SYSERR) {
-		kprintf("SYSERR handled.\n\r");
-	}
-
+          /************ EXAMPLE *****************/              
+                        
+          kprintf("Call 1\r\n");
+	  result = lflistdir(Lf_data.lf_dskdev);
+                        
+	  file = open(LFILESYS, "ABC", "rw");
+	  file = open(LFILESYS, "123", "rw");
+	  file = open(LFILESYS, "DEF", "rw");
+	  file = open(LFILESYS, "456", "rw");
+	
+	  close(file);
+	  
+          kprintf("Call 2\r\n");
+	  result = lflistdir(Lf_data.lf_dskdev);
+          kprintf("Call 3\r\n");
+	  result = lflistdir(-1);
+          kprintf("KillExpect");
+	
+	/************ EXAMPLE *****************/
+	
 	return OK;
 }
